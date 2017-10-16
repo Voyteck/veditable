067402140c3d81c1815d3cecafd7c3c974d522fc
Wojciech Zielinski <Voyteck0@gmail.com>
2017-10-16 13:33:58 +0200

    Slight version change

--- veditable.js
+++ veditable.js
@@ -7,7 +7,7 @@
  * 				http://www.inperitia.com
  * 				https://github.com/Voyteck0/veditable
  * Created:		13.06.2017
- * Version:		0.2.2
+ * Version:		0.2.3
  *
  * Description:
  * Plugin allows to convert inputs or form elements into inline editable elements.
